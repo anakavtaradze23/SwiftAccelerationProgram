514d738c4ca2084b770646900c28a306338de271
anakavtaradze23 <109353590+anakavtaradze23@users.noreply.github.com>
2022-07-15 16:24:25 +0400

    Add files via upload

---
+++ Pupil.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Pupil
+{
+	[Key]
+	public int PupilId { get; set; }
+
+	[Required]
+	public string Name { get; set; }
+
+	[Rquired]
+	public string Surname { get; set; }
+
+	[Required]
+	public string Sex { get; set; }
+
+	[Required]
+	public string Grade { get; set; }
+
+	public List<Teacher> Teacher { get; set; }
+}
---
+++ SchoolDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+public class SchoolDbContext : DbContext
+{
+	public DbSet<Teacher> Teacher { get; set; }
+	public DbSet<Pupil> Pupils { get; set; }
+
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+		optionsBuilder.UseSqlServer(@"Server=ANA;Database=SchoolApp;Trusted_Connection=True;");
+    }
+}
